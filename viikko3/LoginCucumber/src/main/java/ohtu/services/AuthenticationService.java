700a9634db1784110b3e0962d1067b26032d362a
Taru Saloheimo <taru.saloheimo@gmail.com>
2020-02-04 12:41:12 +0200

    logincucumber-tehtävä

--- viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -3,6 +3,8 @@ package ohtu.services;
 import ohtu.domain.User;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import ohtu.data_access.UserDao;
 
 public class AuthenticationService {
@@ -40,7 +42,44 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
+        if (username.length() < 3) {
+            return true;
+        }
+        if (!isUsernameAcceptedLetters(username)) {
+            return true;
+        }
+        if (password.length() < 8) {
+            return true;
+        }
+        if (!passwordContainsLettersAndOtherCharacters(password)) {
+            return true;
+        }
 
         return false;
+    }
+    
+    private boolean isUsernameAcceptedLetters(String username) {
+        username = username.toLowerCase();
+        char[] charArray = username.toCharArray();
+        for (int i = 0; i < username.length(); i++) {
+            char ch = charArray[i];
+            if (!(ch >= 'a' && ch <= 'z')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean passwordContainsLettersAndOtherCharacters(String password) {
+        Pattern letter = Pattern.compile("[a-zA-z]");
+        Pattern digit = Pattern.compile("[0-9]");
+        Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        
+        Matcher hasLetter = letter.matcher(password);
+        Matcher hasDigit = digit.matcher(password);
+        Matcher hasSpecial = special.matcher(password);
+
+        return hasLetter.find() && (hasDigit.find() || hasSpecial.find());
+        
     }
 }
