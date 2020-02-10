bd3f99d3c97d470b98f74de4c5c13542b6d9fc0d
Taru Saloheimo <taru.saloheimo@gmail.com>
2020-02-10 09:22:43 +0200

    Selenium-simulaatiot

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +15,81 @@ public class Tester {
         
         sleep(2);
         
+        //uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("karpo"+r.nextInt(100000));
+
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("salaisuus");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("salaisuus");
+        
+        sleep(2);
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+        
+        sleep(3);
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        
+        
+        //UUDEN KÄYTTÄJÄTUNNUKSEN LUOMINEN
+         
+        /* 
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("kikka"+r.nextInt(100000));
+
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("salaisuus");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("salaisuus");
+        
+        sleep(2);
+
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+        
+        sleep(3);
+        */
+        
+        /*EPÄONNISTUNUT LOGIN: KÄYTTÄJÄTUNNUS OK, SALASANA VÄÄRÄ
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("töpöhäntä");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        */
+        
+        //NORMAALI LOGIN 
+        /*
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -29,7 +105,7 @@ public class Tester {
         element.submit();
 
         sleep(3);
-        
+        */
         driver.quit();
     }
     
